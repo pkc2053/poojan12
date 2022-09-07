8e4a1fb199d235adf7626dd4ebabdd6ec5a96dcf
poojan.1237509@gmail.com <poojan.1237509@gmail.com>
2022-09-07 17:41:00 +0000

    Initial commit

--- models/customers.sql
+++ models/customers.sql
@@ -1,1 +1,1 @@
-SELECT  * FROM `poc-dna-bigbets-foundry-47416.dna_foundry_staging_dataset.Customers_Poojan` LIMIT 1000
+SELECT  * FROM `poc-dna-bigbets-foundry-47416.dna_foundry_staging_dataset.Customers_Poojan` 
